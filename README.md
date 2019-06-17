b9cd87b30d16d112e2bc141904ef677f33d691a5
Stefano Savanelli <stefano@savanelli.it>
2019-06-17 11:51:26 +0200

    aggiunto controllo data di nascita

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Cfita
 
 Check italian fiscal code
+
+see:
+https://www.agenziaentrate.gov.it/wps/content/Nsilib/Nsi/Schede/Istanze/Richiesta+TS_CF/Informazioni+codificazione+pf
+
+
--- lib/cfita/codice_fiscale.rb
+++ lib/cfita/codice_fiscale.rb
@@ -1,21 +1,26 @@
 # frozen_string_literal: true
 
 require 'active_support/all'
+# require './belfiore.rb'
 
 module Cfita
   # Controllo codice fiscale italiano
   class CodiceFiscale
-    attr_reader :codice_fiscale, :data, :errors, :sesso
+    attr_reader :fiscal_code,
+                :sex,
+                :birth_date,
+                :data,
+                :errors
 
-    def initialize(codice_fiscale)
-      @codice_fiscale = codice_fiscale.upcase.strip
+    def initialize(fiscal_code)
+      @fiscal_code = fiscal_code.upcase.strip
       @data = {}
       @errors = []
       parse
     end
 
     def to_s
-      codice_fiscale
+      fiscal_code
     end
 
     def valid?
@@ -32,32 +37,78 @@ module Cfita
       check_checksum
       return if errors.any?
 
-      check_sex
+      check_birth_date
     end
 
     def check_size
-      size = @codice_fiscale.size
+      size = @fiscal_code.size
       errors << "Lunghezza errata (#{size})" unless size == 16
     end
 
     def check_chars
-      test = @codice_fiscale == @codice_fiscale.parameterize.upcase[/^[A-Z0-9]*$/]
+      test = @fiscal_code == @fiscal_code[/^[A-Z0-9]*$/]
       errors << 'Caratteri non ammessi' unless test
     end
 
     def check_checksum
-      errors << 'Checksum errato' if checksum != @codice_fiscale.last
+      errors << 'Checksum errato' if checksum != @fiscal_code.last
     end
 
     def check_sex
-      case @codice_fiscale[9]
+      case @fiscal_code[9]
       when /[0-3LMNP]/
-        @data[:sesso] = 'M'
+        @data[:sex] = 'M'
       when /[4-7QRST]/
-        @data[:sesso] = 'F'
+        @data[:sex] = 'F'
       else
         @errors << 'Cifra decina giorno di nascita errata'
       end
+    end
+
+    MESI = 'ABCDEHLMPRST'.freeze
+
+    def check_birth_date
+      yy = cifre(6..7)
+      return if @errors.any?
+
+      day = cifre(9..10)
+      return if @errors.any?
+
+      @errors << 'Cifra decina giorno di nascita errata' if day > 71
+      return if @errors.any?
+
+      @data[:sex] = day > 40 ? 'F' : 'M'
+
+      month = MESI.index(@fiscal_code[8])
+      @errors << 'Mese errato' unless month
+      return if @errors.any?
+
+      @birth_date =
+        Date.new(yy2yyyy(yy), month + 1, day % 40) rescue nil
+
+      @errors << 'Data di nascita errata' unless @birth_date
+      return if @errors.any?
+
+      @data[:birth_date] = @birth_date
+    end
+
+    def yy2yyyy(yy)
+      Date.today.year -
+        (Date.today.year % 100 + 100 - yy ) % 100
+    end
+
+    def cifre(range)
+      result = 0
+      range.each do |position|
+        char = @fiscal_code[position]
+        value = CIFRE.index(char)
+        @errors << "Carattere '#{char}' errato in posizione #{position}" unless value
+        return nil unless value
+
+        result *= 10
+        result += value % 10
+      end
+      result
     end
 
     DISPARI = [
@@ -69,10 +120,11 @@ module Cfita
     ].freeze
 
     OMOCODICI = 'LMNPQRSTUV'
+    CIFRE = ('0123456789' + OMOCODICI).freeze
 
     def checksum
       tot = 0
-      @codice_fiscale[0..14].bytes.first(15).each.with_index do |byte, i|
+      @fiscal_code[0..14].bytes.first(15).each.with_index do |byte, i|
         next unless byte
 
         byte -= byte < 65 ? 48 : 65
